cf1be5c3122d2f3816442e3ee47ffb0563b72a7c
Michael Wadley <michael@mwdle.com>
2024-04-04 15:55:10 -0600

    revamped outbound messaging queueing solution. Seems to have solved all crashing/memory issues whilst ensuring consistency of the canvas between clients.

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <queue>
+#include <deque>
 
 #define errorLed D0
 
@@ -38,10 +38,11 @@ IPAddress ip(192, 168, 0, 98);
 
 AsyncWebServer server(80);
 
-std::queue<string> queue;
+std::deque<string> queue;
 
 // Initialize display:
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+bool displayChangesQueued = true;
 
 // stringstream pixels;
 
@@ -55,11 +56,24 @@ void recvWebSktMsg(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
     data[len] = 0;
-    string msg;
-    for(size_t i=0; i < info->len; i++) {
-      msg += (char)data[i];
+    string msg((char*)data);
+    int values[7];
+    int index = 0;
+    std::string::const_iterator start = msg.begin();
+    std::string::const_iterator end = msg.end();
+    std::string::const_iterator next = std::find(start, end, ';');
+    while (next != end) {
+        values[index++] = stoi(string(start, next));
+        start = next + 1;
+        next = std::find(start, end, ';');
+        ESP.wdtFeed();
     }
-    queue.push(msg);
+    values[index++] = stoi(string(start, next));
+
+    if (values[0] == 1) display.fillRect(0, 0, 128, 64, BLACK);
+    else display.fillRect(values[3], values[4], values[6], values[6], values[2]);
+    displayChangesQueued = true;
+    queue.push_back(msg);
   }
 }
 
@@ -90,10 +104,10 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
 // Initialize FS
 void initLittleFS() {
   if (!LittleFS.begin()) {
-    Serial.println("An error has occurred while mounting FS");
+    Serial.println("\nAn error has occurred while mounting FS");
   }
   else{
-    Serial.println("FS mounted successfully");
+    Serial.println("\nFS mounted successfully");
   }
 }
 
@@ -151,11 +165,10 @@ void setup(void) {
 
   // Initialize display
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.println("SSD1306 allocation failed");
+    Serial.println("Display intialization failed");
   }
   delay(2000);
   display.clearDisplay();
-  display.display();
 }
 
 void loop(void) {
@@ -165,25 +178,18 @@ void loop(void) {
     lastWifiCheck = millis();
     if (WiFi.status() != WL_CONNECTED) digitalWrite(errorLed, LOW); // Turn on red onboard LED by setting voltage LOW            
     else digitalWrite(errorLed, HIGH); // Turn off red onboard LED by setting voltage HIGH
+  }
+
+  if (displayChangesQueued) {
+    displayChangesQueued = false;
+    display.display();
   } 
 
   if (!queue.empty()) {
-    stringstream msg = stringstream(queue.front());
-    ws.textAll(msg.str().c_str());
-    queue.pop();
-    string values[7];
-    int index = 0;
-    for (string chunk; getline(msg, chunk, ';');) {
-      values[index++] = chunk;
-      ESP.wdtFeed();
-    }
-    if (values[0] == "1") display.fillRect(0, 0, 128, 64, BLACK);
-    else display.fillRect(stoi(values[3]), stoi(values[4]), stoi(values[6]), stoi(values[6]), stoi(values[2]));
-    display.display();
-    ESP.wdtFeed();
+    ws.textAll(queue.front().c_str());
+    delay(40);
+    queue.pop_front();
   }
 
   ws.cleanupClients();
-
-  delay(25);
 }
